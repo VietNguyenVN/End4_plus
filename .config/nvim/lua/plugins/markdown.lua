f1193dab539aab46d817ec4256190eadff6643e5
VietNguyenVN <kvbnv2111@gmail.com>
2026-06-07 22:09:39 +0700

    feat: markdown lint

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      local lint = require("lint")
+      lint.linters["markdownlint-cli2"].args = {
+        "--config",
+        vim.fn.expand("~/.markdownlint.yaml"),
+      }
+      return opts
+    end,
+  },
+}
